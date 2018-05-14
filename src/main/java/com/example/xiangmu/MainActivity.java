710595215bf8f0e59c7baff2c2ee8915b2895477
qikaikaikaikai <qikaikai980324>
2018-05-14 08:07:29 +0800

    啊啊

--- src/main/java/com/example/xiangmu/MainActivity.java
+++ src/main/java/com/example/xiangmu/MainActivity.java
@@ -11,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("TAG", "onCreate:aaaaaa ");
+        Log.d("TAG", "onCreate:祁凯凯 ");
+
+        
     }
 }
